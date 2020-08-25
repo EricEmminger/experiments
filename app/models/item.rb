72929be22a516137198176f13dac3ebbb5d8aeb7
Eric Emminger <EricEmminger@gmail.com>
2020-08-25 15:16:47 -0400

    Sanitize content for plain text and safe html

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,17 @@ class Item < ApplicationRecord
   has_many :syndications
   has_many :feeds, through: :syndications
   has_many :reactions
+
+  def content
+    content_html || content_text || summary
+  end
+
+  def content_plain
+    ActionView::Base.full_sanitizer.sanitize(content)
+  end
+
+  def content_safe
+    safe_list_sanitizer = Rails::Html::SafeListSanitizer.new
+    safe_list_sanitizer.sanitize(content, tags: %w(h2 h3 li ul p), attributes: %w(id class style))
+  end
 end
