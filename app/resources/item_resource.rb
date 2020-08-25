faea7e8d19ffe772a0fe9faf1e6caab819f4613f
Eric Emminger <EricEmminger@gmail.com>
2020-08-25 15:47:13 -0400

    Generate graphiti:resource Item

---
+++ app/resources/item_resource.rb
@@ -0,0 +1,30 @@
+class ItemResource < ApplicationResource
+  has_many :syndications
+  many_to_many :feeds
+  has_many :reactions
+
+  attribute :guid, :string
+  attribute :external_url, :string
+  attribute :title, :string
+  attribute :summary, :string
+  attribute :updated, :datetime
+  attribute :published, :datetime
+  attribute :content, :string
+  attribute :content_plain, :string
+  attribute :content_safe, :string
+  attribute :content_html, :string
+  attribute :content_text, :string
+  attribute :authors, :string
+  attribute :tags, :string
+  attribute :attachments, :string
+
+  filter :profile_id, :integer do |scope, value|
+    eq do |scope, value|
+      scope.joins(feeds: :subscriptions).where(feeds: {subscriptions: {profile_id: value}})
+    end
+  end
+
+  def base_scope
+    Item.order(published: :desc, id: :asc)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
   root 'site#index'
   get 'site/index'
   get 'site/test'
+  namespace :api do
+    namespace :v1 do
+      resources :feeds
+      resources :items
+    end
+  end
 end
