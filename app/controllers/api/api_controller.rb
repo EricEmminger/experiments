ca4cfe25006aaaa6d332b339adcde2be40692904
Eric Emminger <EricEmminger@gmail.com>
2020-09-17 17:07:33 -0400

    Exclude XHR requests from CSRF protection

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,6 +1,5 @@
 module Api
   class ApiController < ApplicationController
     include Graphiti::Rails::Responders
-    skip_before_action :verify_authenticity_token
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :bookmarks
-  devise_for :users
-  resources :feeds
-  resources :items
   root 'site#index'
   get 'site/index'
   get 'site/test'
+  devise_for :users, controllers: {sessions: 'users/sessions'}
+  resources :bookmarks
+  resources :feeds
+  resources :items
   namespace :api do
     namespace :v1 do
       resources :bookmarks
