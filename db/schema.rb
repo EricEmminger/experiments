2e7d5db98e0713bcd310d1db6df1f47b99a5ac57
Eric Emminger <EricEmminger@gmail.com>
2020-09-18 16:57:44 -0400

    Migrate schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_063137) do
+ActiveRecord::Schema.define(version: 2020_07_30_230707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,82 @@ ActiveRecord::Schema.define(version: 2020_05_21_063137) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["tags"], name: "index_bookmarks_on_tags", using: :gin
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
+  end
+
+  create_table "feeds", force: :cascade do |t|
+    t.string "url"
+    t.string "format"
+    t.string "title"
+    t.string "summary"
+    t.string "generator"
+    t.datetime "updated"
+    t.datetime "published"
+    t.text "authors"
+    t.text "tags"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["url"], name: "index_feeds_on_url", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "guid"
+    t.string "external_url"
+    t.string "title"
+    t.string "summary"
+    t.datetime "updated"
+    t.datetime "published"
+    t.text "content_html"
+    t.text "content_text"
+    t.text "authors"
+    t.text "tags"
+    t.text "attachments"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["guid"], name: "index_items_on_guid", unique: true
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "url"
+    t.string "name"
+    t.boolean "private"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["url"], name: "index_profiles_on_url", unique: true
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "reactions", force: :cascade do |t|
+    t.bigint "profile_id", null: false
+    t.bigint "item_id", null: false
+    t.string "kind"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_reactions_on_item_id"
+    t.index ["profile_id"], name: "index_reactions_on_profile_id"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.bigint "profile_id", null: false
+    t.bigint "feed_id", null: false
+    t.boolean "notify"
+    t.boolean "mute"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["feed_id"], name: "index_subscriptions_on_feed_id"
+    t.index ["profile_id", "feed_id"], name: "index_subscriptions_on_profile_id_and_feed_id", unique: true
+    t.index ["profile_id"], name: "index_subscriptions_on_profile_id"
+  end
+
+  create_table "syndications", force: :cascade do |t|
+    t.bigint "feed_id", null: false
+    t.bigint "item_id", null: false
+    t.boolean "shared"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["feed_id", "item_id"], name: "index_syndications_on_feed_id_and_item_id", unique: true
+    t.index ["feed_id"], name: "index_syndications_on_feed_id"
+    t.index ["item_id"], name: "index_syndications_on_item_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -53,4 +129,11 @@ ActiveRecord::Schema.define(version: 2020_05_21_063137) do
   end
 
   add_foreign_key "bookmarks", "users"
+  add_foreign_key "profiles", "users"
+  add_foreign_key "reactions", "items"
+  add_foreign_key "reactions", "profiles"
+  add_foreign_key "subscriptions", "feeds"
+  add_foreign_key "subscriptions", "profiles"
+  add_foreign_key "syndications", "feeds"
+  add_foreign_key "syndications", "items"
 end
